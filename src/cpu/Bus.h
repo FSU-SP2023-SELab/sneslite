dd76b297add2844f54ca511c6cdc3abcc83638c7
Paggigalo <gigliop5000583@gmail.com>
2023-02-20 13:16:21 -0500

    Adding Doxygen comments to Bus.h

--- src/cpu/Bus.h
+++ src/cpu/Bus.h
@@ -12,14 +12,33 @@ namespace sneslite
 	    Bus();
 	    ~Bus();
 
-	    //Devices on the Bus
+	    /**
+	     * @brief Devices on the Bus
+	     * 
+	     */
         Cpu cpu;
 
-	    //Temp fake RAM
+	    /**
+	     * @brief TEMP fake RAM
+	     * 
+	     */
 	    std::array<uint8_t, 64 * 1024> ram;
 
-	    //Bus Read/Write
+	    /**
+	     * @brief Bus Read
+	     * 
+	     * @param addr 
+	     * @param data 
+	     */
 	    void write(uint16_t addr, uint8_t data);
+        
+        /**
+         * @brief Bus Write
+         * 
+         * @param addr 
+         * @param bReadOnly 
+         * @return uint8_t 
+         */
 	    uint8_t read(uint16_t addr, bool bReadOnly = false);
 
     };
