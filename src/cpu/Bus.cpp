580400237d5f2ae3270b87f24d053e79f5741818
Jack Brower <ejbrower4@gmail.com>
2023-03-24 14:20:58 -0400

    Remove redundant bounds check

--- src/cpu/Bus.cpp
+++ src/cpu/Bus.cpp
@@ -23,16 +23,13 @@ namespace sneslite
 
     void Bus::write(uint16_t addr, uint8_t data)
     {
-	    if (addr >= 0x0000 && addr <= 0xFFFF)
-		    ram[addr] = data;
+		ram[addr] = data;
     }
 
     uint8_t Bus::read(uint16_t addr, bool bReadOnly)
     {
-	    if (addr >= 0x0000 && addr <= 0xFFFF)
-		    return ram[addr];
+		return ram[addr];
 
-	    return 0x00;
     }
 
     void Bus::SetSampleFrequency(uint32_t sample_rate)
