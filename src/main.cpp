ee5a37402f0eb0e4efbcd34217e13b1794629f62
smt19f <smt19f@fsu.edu>
2023-04-03 12:27:22 -0400

    quick main and make changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,25 @@
 #include <iostream>
 #include <cstdint>
-//#include <SFML/Graphics.hpp>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include <SFML/System.hpp>
 
 int main()
 {
-    // sf::RenderWindow window(sf::VideoMode(800, 600), "SNESlite");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "SNESlite");
 
-    // while (window.isOpen()) {
+    while (window.isOpen()) {
 
-    //     sf::Event event;
+        sf::Event event;
 
-    //     while (window.pollEvent(event)) {
-    //         if (event.type == sf::Event::Closed) {
-    //             window.close();
-    //         }
-    //     }
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                window.close();
+            }
+        }
 
-    //     window.clear();
-    //     window.display();
-    // }
+        window.clear();
+        window.display();
+    }
     return 0;
 }
