a2aa557d1ae47cdfe48fbd3000d200f82d4a41e2
Jack Brower <ejbrower4@gmail.com>
2023-04-28 14:49:23 -0400

    Modify cart test

--- tests/cartridge.h
+++ tests/cartridge.h
@@ -6,7 +6,4 @@
 bool test_cartridge(std::string file)
 {
     std::cout << "Testing cartridge read..." << std::endl;
-
-    sneslite::Cartridge test_cart;
-    return test_cart.load_dump_file(file);
 }
