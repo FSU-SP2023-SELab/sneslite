2a27d49b8707a2d0f573a6c3602b06992051ce19
smt19f <amt19f.fse.edu>
2023-04-30 13:24:58 -0400

    move test_rom to own file

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
+#include <fstream>
 #include <cstdint>
+#include <string>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
 #include <SFML/System.hpp>
 
+#include "cpu/Bus.h"
+#include "tests/test_rom.hpp"
+
 int main()
 {
     sf::RenderWindow window(sf::VideoMode(800, 600), "SNESlite");
+
+    // Create system
+    sneslite::Bus nes;
+
+    // Build rom
+    std::string rom  = "./build/romtest.nes";
+    {
+        std::ofstream output(rom, std::ios::binary);
+        output.write(reinterpret_cast<const char*>(&ROM_TEST), sizeof(ROM_TEST));
+    }
+
 
     while (window.isOpen()) {
 
