c9e02331176019d8de6802c7daa8fb11367af12a
Jack Brower <ejbrower4@gmail.com>
2023-05-01 16:58:02 -0400

    Refactor

--- tests/ppu.h
+++ tests/ppu.h
@@ -5,28 +5,26 @@
 #include<fstream>
 #include<vector>
 #include<SFML/Graphics.hpp>
-#include <SFML/System.hpp>
-#include <SFML/Window.hpp>
+#include<SFML/System.hpp>
+#include<SFML/Window.hpp>
 
 bool test_ppu(std::string file, sneslite::Bus bus)
 {
     // initialize SFML and create a window
     sf::RenderWindow window(sf::VideoMode(800, 600), "TestROM");
 
-    sf::Texture texture;
-    sf::Sprite sprite;
-
-    auto& ppu_frame = bus.ppu.p_f;
-    auto tile_frame = ppu_frame->show_tile(bus.cartridge.get_char_rom(), 1, 0);
-
-    texture.create(8, 8);
-    texture.update(tile_frame.data(), 8, 8, 0, 0);
-
-    sprite.setTexture(texture);
-    sprite.setPosition(0, 0);
-
     while (window.isOpen())
     {
+        bus.ppu.p_f->render();
+
+        sf::Texture texture;
+
+        auto& ppu_frame = bus.ppu.p_f;
+        auto tile_frame = ppu_frame->show_tile(bus.cartridge.get_char_rom(), 1, 0);
+
+        texture.create(256, 240);
+        texture.update(tile_frame.data());
+
         // handle events
         sf::Event event;
         while (window.pollEvent(event))
@@ -38,6 +36,7 @@ bool test_ppu(std::string file, sneslite::Bus bus)
         }
 
         // clear the window and draw the sprite
+        sf::Sprite sprite(texture);
         window.clear();
         window.draw(sprite);
         window.display();
